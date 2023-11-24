dbb2af2eba82241237de5ee52ef40aef7cd20674
tylerpiercey <122289319+tylerpiercey@users.noreply.github.com>
2023-11-23 23:19:15 -0330

    project start

---
+++ src/main/java/com/example/cp3566project/Cp3566ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cp3566project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp3566ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp3566ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cp3566project/Cp3566ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cp3566project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp3566ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
